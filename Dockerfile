0f4bb2039a995bb2de4406b4c3b45645b5732f1d
Oscar Nord <oscar.nord@gmail.com>
2021-12-30 13:57:26 +0100

    chore: add docker-compose/dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:slim
+
+WORKDIR /app
+
+ADD . .
+
+RUN npm install
+
+RUN npm run build
+
+CMD ["npm", "run", "dev"]
--- dev.ts
+++ dev.ts
@@ -2,7 +2,7 @@ import { Worker } from './index';
 import Logger from './logging/logger';
 
 // ===For Demo===
-const NUMBER_OF_WORKERS: number = 2;
+const NUMBER_OF_WORKERS: number = process.env.NUMBER_OF_WORKERS ? parseInt(process.env.NUMBER_OF_WORKERS) : 2;
 const myWorkers: Worker[] = [];
 // Recruit Workers
 for (let i = 0; i < NUMBER_OF_WORKERS; i++) {
